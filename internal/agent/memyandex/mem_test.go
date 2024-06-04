a59994b733073d22b2b73591ee1c78ac4e997a45
DenisquaP <timchamkin1@yandex.ru>
2024-06-04 22:34:51 +0300

    fix test

--- internal/agent/memyandex/mem_test.go
+++ internal/agent/memyandex/mem_test.go
@@ -1,16 +1,16 @@
 package memyandex
 
 import (
-	"github.com/stretchr/testify/assert"
 	"runtime"
 	"testing"
-	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMemStatsYaSt_UpdateMetrics(t *testing.T) {
 	m := MemStatsYaSt{RuntimeMem: &runtime.MemStats{}}
 
-	m.UpdateMetrics(2 * time.Second)
+	m.UpdateMetrics(2)
 
 	// Проверка что хотя бы 1 переменная обновилась
 	assert.NotEmpty(t, m.RuntimeMem.Alloc)
