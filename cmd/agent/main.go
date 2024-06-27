4ae5e1a2ef26da60a698f8b1c263f8bc1f0c9542
DenisquaP <timchamkin1@yandex.ru>
2024-06-27 10:55:15 +0300

    add sleep to agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/DenisquaP/ya-metrics/internal/agent"
 )
 
 func main() {
+	time.Sleep(5 * time.Second)
 	log.Println("agent run")
 	agent.Run()
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"time"
-
 	"github.com/DenisquaP/ya-metrics/internal/server"
 )
 
 func main() {
-	time.Sleep(11 * time.Second)
 	server.Run()
 }
