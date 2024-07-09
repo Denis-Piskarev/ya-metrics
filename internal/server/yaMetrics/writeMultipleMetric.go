1159c8da20c6d8e07bbbd54a9c34f68711ec65a1
Piskarev Denis <dapiskarev@get-lab.ru>
2024-07-09 11:20:34 +0300

    fix typo

--- internal/server/yaMetrics/writeMultipleMetric.go
+++ internal/server/yaMetrics/writeMultipleMetric.go
@@ -6,17 +6,17 @@ import (
 	"github.com/DenisquaP/ya-metrics/pkg/models"
 )
 
-func (y *MemStorage) WriteMetrics(ctx context.Context, metrics []*models.Metrics) error {
+func (m *MemStorage) WriteMetrics(ctx context.Context, metrics []*models.Metrics) error {
 	for _, metric := range metrics {
 		switch metric.MType {
 		case "gauge":
-			y.Gauge[metric.ID] = *metric.Value
+			m.Gauge[metric.ID] = *metric.Value
 		case "counter":
-			met, ok := y.Counter[metric.ID]
+			met, ok := m.Counter[metric.ID]
 			if !ok {
-				y.Counter[metric.ID] = *metric.Delta
+				m.Counter[metric.ID] = *metric.Delta
 			} else {
-				y.Counter[metric.ID] = met + *metric.Delta
+				m.Counter[metric.ID] = met + *metric.Delta
 			}
 		}
 	}
