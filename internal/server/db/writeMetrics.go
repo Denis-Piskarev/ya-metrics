aac9f6949ca98f493b39a7f39c8b3f3a027e6ad7
Piskarev Denis <dapiskarev@get-lab.ru>
2024-07-09 10:32:16 +0300

    fixed write multiple errors in db

--- internal/server/db/writeMetrics.go
+++ internal/server/db/writeMetrics.go
@@ -2,12 +2,14 @@ package db
 
 import (
 	"context"
+	"errors"
 
 	"github.com/jackc/pgx/v5"
 
 	"github.com/DenisquaP/ya-metrics/pkg/models"
 )
 
+// WriteMetrics writes multiple metrics into db
 func (d *DB) WriteMetrics(ctx context.Context, metrics []*models.Metrics) error {
 	// Beginning transaction
 	tx, err := d.DB.BeginTx(ctx, pgx.TxOptions{})
@@ -22,6 +24,27 @@ func (d *DB) WriteMetrics(ctx context.Context, metrics []*models.Metrics) error 
 	query := `INSERT INTO metrics (type, name, counter, gauge) VALUES ($1, $2, $3, $4)`
 
 	for _, metric := range metrics {
+		// If metric type is counter we need to get old value
+		if metric.MType == "counter" {
+			var oldCounter int64
+			if err := tx.QueryRow(ctx, `SELECT counter FROM metrics WHERE name=$1 AND type = 'counter'`, metric.ID).Scan(&oldCounter); err != nil {
+				// If no rows we insert new value
+				if errors.Is(err, pgx.ErrNoRows) {
+					if _, err := tx.Exec(ctx, query, metric.MType, metric.ID, metric.Delta, metric.Value); err != nil {
+						d.Logger.Errorw("write new counter error", "error", err)
+						return err
+					}
+
+					continue
+				}
+
+				d.Logger.Errorw("error get old counter", "error", err)
+				return err
+			}
+
+			// Add old value
+			*metric.Delta += oldCounter
+		}
 		if _, err := tx.Exec(ctx, query, metric.MType, metric.ID, metric.Delta, metric.Value); err != nil {
 			d.Logger.Errorw("write metrics error", "error", err)
 			return err
